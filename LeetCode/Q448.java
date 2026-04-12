1a34ee8ebb7239b72a452bdd85332d0cb423da62
Hadi Ahmed <969hadiahmed@gmail.com>
2026-04-12 14:28:50 +0530

    LEET CODE: 448: optimiszed
    Runtime 4 ms Beats 96.78%
    Memory 70.57 MB Beats 39.42%

--- LeetCode/Q448.java
+++ LeetCode/Q448.java
@@ -2,11 +2,11 @@
 
 // Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
-// Runtime 5 ms Beats 84.72% 
-// Memory 70.39 MB Beats 42.83%
+// Runtime 4 ms Beats 96.78%
+// Memory 70.57 MB Beats 39.42%
+
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Q448 {
@@ -15,17 +15,14 @@ public class Q448 {
         int[] arr = new int[n + 1];
 
         ArrayList<Integer> missing = new ArrayList<>();
-        Arrays.fill(arr, -1);
-
         for (int i = 0; i < n; i++) {
-            arr[nums[i]] = nums[i];
+            arr[nums[i]]++;;
         }
 
         for (int i = 1; i < n + 1; i++) {
-            if (arr[i] == -1)
+            if (arr[i] < 1)
                 missing.add(i);
         }
-
         return missing;
     }
 }
