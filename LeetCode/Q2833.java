9ec4f77b343f84e38750a9fa8a5502419e361d21
Hadi Ahmed <969hadiahmed@gmail.com>
2026-04-24 19:39:20 +0530

    LEET CODE: 2833
    2833. Furthest Point From Origin
    
    You are given a string moves of length n consisting only of characters 'L', 'R', and '_'. The string represents your movement on a number line starting from the origin 0.
    In the ith move, you can choose one of the following directions:
    move to the left if moves[i] = 'L' or moves[i] = '_'
    move to the right if moves[i] = 'R' or moves[i] = '_'
    Return the distance from the origin of the furthest point you can get to after n moves.
    
    Runtime 1 ms Beats 100.00%
    Memory 43.88 MB Beats 45.71%

---
+++ LeetCode/Q2833.java
@@ -0,0 +1,29 @@
+// 2833. Furthest Point From Origin
+
+// You are given a string moves of length n consisting only of characters 'L', 'R', and '_'. The string represents your movement on a number line starting from the origin 0.
+// In the ith move, you can choose one of the following directions:
+// move to the left if moves[i] = 'L' or moves[i] = '_'
+// move to the right if moves[i] = 'R' or moves[i] = '_'
+// Return the distance from the origin of the furthest point you can get to after n moves.
+
+// Runtime 1 ms Beats 100.00%
+// Memory 43.88 MB Beats 45.71%
+
+class Q2833 {
+    public int furthestDistanceFromOrigin(String moves) {
+        int pos = 0;
+        int count = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'R')
+                pos++;
+            else if (c == 'L')
+                pos--;
+            else
+                count++;
+        }
+        if(pos>=0) pos += count;
+        else pos -= count;
+    
+        return pos > 0 ? pos : -pos;
+    }
+}
