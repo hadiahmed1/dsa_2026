a500565a1ad3189f655233185f434c37cd29036a
Hadi Ahmed <969hadiahmed@gmail.com>
2026-04-30 22:21:36 +0530

    LEET CODE: 9

---
+++ LeetCode/Q9.java
@@ -0,0 +1,16 @@
+public class Q9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int n = x;
+        int rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+
+        return x == rev;
+    }
+}
