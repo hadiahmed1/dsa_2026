0ea5c64440694936f0a5dea0ede025c2bc84d6ae
Hadi Ahmed <969hadiahmed@gmail.com>
2026-05-02 17:50:09 +0530

    LEET CODE: 788
    Q788. Rotated Digits
     Runtime 3 ms Beats 91.63%
    Memory 41.8 MB Beats 96.61%

---
+++ LeetCode/Q788.java
@@ -0,0 +1,31 @@
+// Q788. Rotated Digits
+
+// Runtime 3 ms Beats 91.63%
+// Memory 41.8 MB Beats 96.61%
+
+import java.util.*;
+
+class Q788 {
+    static boolean isGood(int n) {
+        boolean rotDiff = false;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            if (d == 3 || d == 4 || d == 7)
+                return false;
+            if (d == 2 || d == 5 || d == 6 || d == 9)
+                rotDiff = true;
+        }
+
+        return rotDiff;
+    }
+
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for(int i = 1; i<= n; i++){
+            if(isGood(i)) count++;
+        }
+
+        return count;
+    }
+}
