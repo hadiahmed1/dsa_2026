e35c7a05866db24e51df7b5de0e47c51b875078a
Hadi Ahmed <969hadiahmed@gmail.com>
2026-05-02 13:18:00 +0530

    LEET CODE: 9
    Q9. Palindrome Number
     Runtime 5 ms Beats 79.65%
    Memory 45.8 MB Beats 72.40%

--- LeetCode/Q9.java
+++ LeetCode/Q9.java
@@ -1,16 +1,23 @@
-public class Q9 {
+// Q9. Palindrome Number
+
+// Runtime 5 ms Beats 79.65%
+// Memory 45.8 MB Beats 72.40%
+
+import java.util.*;
+
+class Q9 {
     public boolean isPalindrome(int x) {
         if (x < 0)
             return false;
-
+        
         int n = x;
         int rev = 0;
 
-        while (n > 0) {
-            rev = rev * 10 + n % 10;
-            n = n / 10;
+        while(n>0) {
+            rev = rev*10 + n%10;
+            n= n/10;
         }
 
-        return x == rev;
+        return x==rev; 
     }
 }
