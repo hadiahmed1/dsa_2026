09a8719a1e4f3a7bbee64af1bea577dd20a6e5dc
Hadi Ahmed <969hadiahmed@gmail.com>
2026-05-02 13:18:00 +0530

    LEET CODE: 14
    Q14. Longest Common Prefix
     Runtime 2 ms Beats 17.67%
    Memory 43.1 MB Beats 82.37%

--- LeetCode/Q14.java
+++ LeetCode/Q14.java
@@ -1,4 +1,9 @@
-// 14. Longest Common Prefix
+// Q14. Longest Common Prefix
+
+// Runtime 2 ms Beats 17.67%
+// Memory 43.1 MB Beats 82.37%
+
+import java.util.*;
 
 class Q14 {
     public String longestCommonPrefix(String[] strs) {
