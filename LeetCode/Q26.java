4ab8f969dc7ca50f72e49783db84a28d41e58dc7
Hadi Ahmed <969hadiahmed@gmail.com>
2026-05-02 13:18:00 +0530

    LEET CODE: 26
    Q26. Remove Duplicates from Sorted Array
     Runtime 0 ms Beats 100.00%
    Memory 46.5 MB Beats 80.40%

--- LeetCode/Q26.java
+++ LeetCode/Q26.java
@@ -1,18 +1,16 @@
-// 26. Remove Duplicates from Sorted Array
+// Q26. Remove Duplicates from Sorted Array
 
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
-// Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
-// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+// Runtime 0 ms Beats 100.00%
+// Memory 46.5 MB Beats 80.40%
 
-// Runtime 0 ms Beats 100.00% 
-// Memory 46.52 MB Beats 81.31%
+import java.util.*;
 
-public class Q26 {
+class Q26 {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
         int index = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[index]) {
+        for(int i = 1; i < n; i++) {
+            if(nums[i]!=nums[index]){
                 nums[++index] = nums[i];
             }
         }
