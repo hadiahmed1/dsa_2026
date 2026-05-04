8b84a2852d82735963d0df48a989815ca19c1178
Hadi Ahmed <969hadiahmed@gmail.com>
2026-05-04 21:29:23 +0530

    LEET CODE: 2033
    Q2033. Minimum Operations to Make a Uni-Value Grid
     Runtime 33 ms Beats 83.08%
    Memory 111.3 MB Beats 34.66%

--- LeetCode/Q2033.java
+++ LeetCode/Q2033.java
@@ -1,7 +1,7 @@
 // Q2033. Minimum Operations to Make a Uni-Value Grid
 
-// Runtime 33 ms Beats 83.09%
-// Memory 111.3 MB Beats 34.70%
+// Runtime 33 ms Beats 83.08%
+// Memory 111.3 MB Beats 34.66%
 
 import java.util.*;
 
